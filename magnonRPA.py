39b191ea62bb2dc8a6111348cf7c17e98c74fdba
Jonathan Curtis <jon.curtis.94@gmail.com>
2025-03-26 17:58:09 +0100

    Implemented RPA calculations and now implementing magnon kernel diagonalization. Double check that the momenta are passed in the -pi,pi interval or implement this.

--- magnonRPA.py
+++ magnonRPA.py
@@ -28,8 +28,8 @@ plt.rc('lines', linewidth=2.5)
 
 
 S = 0.5
-zero = 1.e-8
 t = 1. ### We will use units throughout with t = 1 for the time being 
+zero = 1.e-5*t
 
 rng = np.random.default_rng()
 
@@ -49,8 +49,8 @@ def B1g(kx,ky):
 	return np.sin(kx)*np.sin(ky)
 
 ### Fermi Dirac function using tanh form 
-def fd(w,T):
-	return 0.5*np.tanh(0.5*w/T)
+def fd(w,mu,T):
+	return 0.5*np.tanh(0.5*(w-mu)/T)
 
 ### Computes spectral function from retarded GF 
 def G2A(G):
@@ -69,6 +69,26 @@ def load_hole_spectrum(fpath):
 
 	return kxs,kys,np.real(ws),G2A(G)
 
+
+###################
+### Hole Doping ###
+###################
+
+### This computes the doping as a function of chemical potential in order to invert and find the chemical potential
+def calc_density(kxs,kys,ws,A,mu,T):
+	### First we generate the tensor describing the filling factor 
+	fd_tensor = gen_fd_tensor(kxs,kys,ws,mu,T)
+
+	### The fd tensor is 0.5 tanh(x/2T)
+	### We want 0.5- fd_tensor = 0.5-0.5tanh(x/2) = 1/(e^x + 1)
+	occ_tensor = 0.5*np.ones_like(fd_tensor) - fd_tensor
+
+	dw = ws[1]-ws[0]
+	dkx = ( (kxs[1]-kxs[0])/float(len(kxs)) )
+	dky = ( (kys[1]-kys[0])/float(len(kys)) )
+	return np.mean(occ_tensor*A)### This should be the density 
+
+
 ######################
 ### Calculating Pi ###
 ######################
@@ -78,11 +98,10 @@ def gen_A1g_tensor(kxs,kys,ws):
 	return A1g(kxv,kyv)
 
 ### This method returns a tensor of the FD function at the corresponding energies 
-def gen_fd_tensor(kxs,kys,ws,T):
+def gen_fd_tensor(kxs,kys,ws,mu,T):
 	kxv,kyv,wv = np.meshgrid(kxs,kys,ws)
 
-	return fd(wv,T)
-
+	return fd(wv,mu,T)
 
 ### Pi is computed as a convolution of the two spectral functions.
 ### We have imaginary part given by 
@@ -92,7 +111,7 @@ def gen_fd_tensor(kxs,kys,ws,T):
 ### QUESTION: Is it ok to use mode='same' for the convolutions? 
 ### This has the effect of chopping the final convolution down to the size of the input arrays but it is unclear if this is the correct boundary condition for the momentum which should be periodic to capture Umklapp process
 ### It might be correct for frequency which should then be suffciently padded outside of the holes bandwidth
-def calc_ImPi(kxs,kys,ws,A,T):
+def calc_ImPi(kxs,kys,ws,A,mu,T):
 
 	Nkx = len(kxs)
 	Nky = len(kys)
@@ -102,7 +121,7 @@ def calc_ImPi(kxs,kys,ws,A,T):
 	### First we construct the frist vector in the convolution
 	### Form factor tensors  
 	A1g_tensor = gen_A1g_tensor(kxs,kys,ws)
-	fd_tensor = gen_fd_tensor(kxs,kys,ws,T)
+	fd_tensor = gen_fd_tensor(kxs,kys,ws,mu,T)
 
 	### First we compute the term with f on the first spectral function 
 	vec1 = A1g_tensor*A1g_tensor*A
@@ -136,12 +155,75 @@ def Kramers_Kronig(ws,Im_part):
 	kk_matrix = 1./(np.pi*float(Nws))*1./(np.tensordot(ws,np.ones_like(ws),axes=-1) - np.tensordot(np.ones_like(ws),ws,axes=-1) - 1.j*zero )
 	return Im_part@kk_matrix
 
+########################################
+### Computing Magnon Greens Funciton ###
+########################################
+
+### Accepts momentum and frequency and J and returns spin wave matrix kernel 
+### returns (w tau_3 zeta_3 - K_q ) for w -> w+i0^+
+### Shape is [4,4,Nkx,Nky,Nw] may be quite large 
+def LSW_kernel(kxs,kys,ws,J):
+	Nkx = len(kxs)
+	Nky = len(kys)
+	Nw = len(ws)
+
+	kxvs,kyvs,wvs = np.meshgrid(kxs,kys,ws)
+
+	kernel = np.zeros((4,4,Nkx,Nky,Nw),dtype=complex)
+
+	for i in range(4):
+		if i < 2:
+			kernel[i,i,...] = (wvs+1.j*zero*np.ones_like(wvs))*(-1)**i-4.*S*J*np.ones_like(wvs)
+		if i >= 2:
+			kernel[i,i] = -(wvs+1.j*zero*np.ones_like(wvs))*(-1)**i-4.*S*J*np.ones_like(wvs)
+
+	a1g = gen_A1g_tensor(kxs,kys,ws)
+	kernel[0,1] = -4.*S*J*a1g
+	kernel[1,0] = -4.*S*J*a1g
+	kernel[2,3] = -4.*S*J*a1g
+	kernel[3,2] = -4.*S*J*a1g
+
+	return kernel 
+
+### This will include the Pi matrices in the computation of the magnon kernel 
+### Returns same signature as bare kernel
+### Pis are passed individually 
+### We must be careful as they may not be symmetric in frequency or momentum 
+def RPA_kernel(kxs,kys,ws,Pi0,Pi1,J):
+	kernel = LSW_kernel(kxs,kys,ws,J)
+
+	### Now we place the Pi components
+	for i in range(2):
+		kernel[i,i] += -Pi0 
+		kernel[i,2] += -Pi1
+		kernel[i,3] += -Pi1
+
+	kernel[0,1] += -Pi0
+	kernel[1,0] += -Pi0
+
+	for i in range(2):
+		kernel[2+i,2+i] += -np.flip(Pi0) ### We need -q components 
+		kernel[2+i,0] += -Pi1
+		kernel[2+i,1] += -Pi1
+
+	kernel[2,3] += -np.flip(Pi0)
+	kernel[3,2] += -np.flip(Pi0)
+
+	return kernel
+
+
+
+
+### This gives the Bogoliubov magnon propagator as a matrix 
+### The matrix elements are for the Bogoliubov spinor (a_q,b_-q^+, a_-q^+, b_q) components 
+
 
 
 ##########################################
 ### DEPRECATED METHODS FOR MONTE CARLO ###
 ##########################################
 ### This method uniformly samples the momentum points 
+"""
 def gen_kpts_uniform(ps):
 	N = int(ps.NMC)
 	kpts = rng.random((2,N))*2.*np.pi-np.pi*np.ones((2,N)) ### Random k points 
@@ -251,6 +333,7 @@ def ImPi_quad(q,ps):
 	integrand_2 = lambda w,kx,ky: 2.*np.pi*S*ps.t**2*Ag(np.array([kx,ky]))*Ag(np.array([kx,ky]) + q[1:])*hole_spectrum(np.array([w,kx,ky]),ps)*hole_spectrum(np.array([w,kx,ky])+q,ps)*( 0.5*np.tanh(0.5*w/ps.T) - 0.5*np.tanh(0.5*(w+q[0])/ps.T) )
 
 	return np.array([ intg.tplquad(integrand_1,-np.pi,np.pi,-np.pi,np.pi,-ps.Emax,ps.Emax)[0]/(4.*np.pi**2) , intg.tplquad(integrand_2,-np.pi,np.pi,-np.pi,np.pi,-ps.Emax,ps.Emax)[0]/(4.*np.pi**2) ])
+"""
 
 def main():
 
@@ -262,13 +345,50 @@ def main():
 	dataDirectory = "../data/"
 
 	holesDirectory = dataDirectory+"hole_spectra/03252025/"
-	holesFile = "Hole_Spectral_functionJz0.05_alfa0.999_Nx20_Ny20"
+	holesFile = "Hole_Spectral_functionJz0.0_alfa0.999_Nx20_Ny20"
 
 	kxs,kys,ws,A = load_hole_spectrum(holesDirectory+holesFile)
 
-	ImPi0,ImPi1 = calc_ImPi(kxs,kys,ws,A,T)
+	A = np.abs(A) ### Sign errors in spectral function 
+	print(np.mean(A)) ### Should equal 1 if normalized 
+
+	#plt.imshow(np.transpose(A[:,0,:]),origin='lower',extent=[kxs[0],kxs[-1],ws[0],ws[-1]],aspect=0.3)
+	#plt.colorbar()
+	#plt.show()
+
+	nmus = 100
+	mus = np.linspace(-20.*t,20.*t,nmus)
+	deltas = np.zeros_like(mus)
+	for i in range(nmus):
+		deltas[i] = calc_density(kxs,kys,ws,A,mus[i],T)
+
+	#plt.plot(mus,deltas)
+	#plt.xlabel(r'$\mu/t$')
+	#plt.ylabel(r'$\delta$')
+	#plt.show()
+
+	mu = -3.8*t
+	ImPi0,ImPi1 = calc_ImPi(kxs,kys,ws,A,mu,T)
 	Pi0 = Kramers_Kronig(ws,ImPi0)
 	Pi1 = Kramers_Kronig(ws,ImPi1)
+
+	magnon_kernel = LSW_kernel(kxs,kys,ws,J)
+	print(magnon_kernel.shape)
+	magnon_prop = np.zeros_like(magnon_kernel)
+
+	for i in range(len(kxs)):
+		for j in range(len(kys)):
+			for k in range(len(ws)):
+				magnon_prop[:,:,i,j,k] = np.linalg.inv(magnon_kernel[:,:,i,j,k])
+
+	plt.imshow(np.transpose(-1./np.pi*np.imag(np.trace(magnon_prop[:,:,:,0,:]) )),origin='lower', extent=[kxs[0],kxs[-1],ws[0],ws[-1]],aspect=1.,cmap='coolwarm')
+	plt.colorbar()
+	plt.xlabel(r'$q_x$')
+	plt.ylim(-2.*J,2.*J)
+	plt.ylabel(r'$\omega/t$')
+	plt.show()
+	quit()
+
 
 	indices = [ [0,0],[10,0],[10,10],[-1,10],[-1,-1] ]
 
@@ -299,7 +419,7 @@ def main():
 	plt.xlabel(r'$\omega/t$')
 	plt.ylabel(r'Re$\Pi_1(\omega,\mathbf{q})/t$')
 	plt.legend()
-	plt.savefig(figDirectory+"/RePi0.pdf",bbox_inches='tight')
+	plt.savefig(figDirectory+"/RePi1.pdf",bbox_inches='tight')
 	plt.show()
 
 	for i in indices:
@@ -309,7 +429,7 @@ def main():
 	plt.xlabel(r'$\omega/t$')
 	plt.ylabel(r'Im$\Pi_1(\omega,\mathbf{q})/t$')
 	plt.legend()
-	plt.savefig(figDirectory+"/ImPi0.pdf",bbox_inches='tight')
+	plt.savefig(figDirectory+"/ImPi1.pdf",bbox_inches='tight')
 	plt.show()
 
 
