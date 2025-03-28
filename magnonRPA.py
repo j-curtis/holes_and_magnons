3b24f11e21591604e1e6b7a664d741737e3ecdc1
Jonathan Curtis <jon.curtis.94@gmail.com>
2025-03-28 21:58:07 +0100

    Fixed reconstruction isseus with Kramers Kronig and now real part is reconstructed more accurately.

--- magnonRPA.py
+++ magnonRPA.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import pickle as pkl
+import scipy as scp
 from scipy import integrate as intg
 from scipy import signal 
 import time
@@ -29,7 +30,6 @@ plt.rc('lines', linewidth=2.5)
 
 S = 0.5
 t = 1. ### We will use units throughout with t = 1 for the time being 
-zero = 1.e-5*t
 
 rng = np.random.default_rng()
 
@@ -63,12 +63,20 @@ def G2A(G):
 ### Loads the numpy pickle files for the hole spectra from Ivan at the specified path location 
 ### Returns the spectral function arrays kx, ky, w, G as arrays
 ### The signature of the returned array is rank 3 tensor with indices [kx,ky,w]
-def load_hole_spectrum(fpath):
+def load_hole_spectrum(fpath,roll_momenta=False):
 	with open(fpath,'rb') as f:
 		kxs,kys,ws,G = pkl.load(f)
 
-	return kxs,kys,np.real(ws),G2A(G)
+	if roll_momenta:
+		### we roll the first two axes and the momenta so they are from -pi to pi using np.fft.fftshift
 
+		kxs = kxs - np.pi+kxs[1] -kxs[0]
+		kys = kys - np.pi+kys[1] -kys[0]
+
+		G = np.fft.fftshift(G,axes=[0,1])
+
+
+	return kxs,kys,np.real(ws),np.abs(G2A(G)) ### We need to take abs of spectral function which is directly computed form time-ordered G and needs to be converted to retarded
 
 ###################
 ### Hole Doping ###
@@ -86,8 +94,7 @@ def calc_density(kxs,kys,ws,A,mu,T):
 	dw = ws[1]-ws[0]
 	dkx = ( (kxs[1]-kxs[0])/float(len(kxs)) )
 	dky = ( (kys[1]-kys[0])/float(len(kys)) )
-	return np.mean(occ_tensor*A)### This should be the density 
-
+	return np.mean(occ_tensor*A)*dw*len(ws) ### This should be the density 
 
 ######################
 ### Calculating Pi ###
@@ -116,6 +123,7 @@ def calc_ImPi(kxs,kys,ws,A,mu,T):
 	Nkx = len(kxs)
 	Nky = len(kys)
 	Nw = len(ws)
+	dw = ws[1] - ws[0]
 	#ImPi = np.zeros((2,Nkx,Nky,Nw))
 
 	### First we construct the frist vector in the convolution
@@ -129,10 +137,13 @@ def calc_ImPi(kxs,kys,ws,A,mu,T):
 	vec3 = A1g_tensor*A1g_tensor*fd_tensor*A
 	vec4 = A 
 
-	mode_conv = 'same'
+	conv_mode = 'same'
+	
+	### We want periodic boundary conditions for the convolutions but this will be challenging to implement in d = 3 
+	### The native convolve2d method does not work but FFT convolve should work 
 
-	ImPi0 = signal.convolve(vec1,np.flip(vec2),mode=mode_conv) - signal.convolve(vec3,np.flip(vec4),mode=mode_conv)
-	ImPi0[:,:,:] *= 2.*np.pi*S*t**2/float(Nkx*Nky*Nw) ### The integrals are normalized by total number of points 
+	ImPi0 = signal.fftconvolve(vec1,np.flip(vec2),mode=conv_mode) - signal.fftconvolve(vec3,np.flip(vec4),mode=conv_mode)
+	ImPi0[:,:,:] *= 2.*np.pi*S*t**2*dw/float(Nkx*Nky) ### The momentum integrals are normalized by total number of points, energy by the differential
 
 	### Now we repeat but for Pi1 we have slightly different form factor assignments in the convolution 
 
@@ -140,8 +151,8 @@ def calc_ImPi(kxs,kys,ws,A,mu,T):
 	vec1 = A1g_tensor*A
 	vec2 = fd_tensor*A1g_tensor*A
 
-	ImPi1 = signal.convolve(vec1,np.flip(vec2),mode=mode_conv) - signal.convolve(vec2,np.flip(vec1),mode=mode_conv)
-	ImPi1[:,:,:] *= 2.*np.pi*S*t**2/float(Nkx*Nky*Nw) ### The integrals are normalized by total number of points 
+	ImPi1 = signal.fftconvolve(vec1,np.flip(vec2),mode=conv_mode) - signal.fftconvolve(vec2,np.flip(vec1),mode=conv_mode)
+	ImPi1[:,:,:] *= 2.*np.pi*S*t**2*dw/float(Nkx*Nky) ### The momentum integrals are normalized by total number of points, energy by the differential
 
 	return ImPi0,ImPi1
 
@@ -149,11 +160,19 @@ def calc_ImPi(kxs,kys,ws,A,mu,T):
 ### PiR[i,j,k] = 1./ (pi N) sum_l Im_part[i,j,l] 1./(ws[l] - ws[k] - i0^+) 
 ### This can make use of np.dot which sums the last axis of the first array with the (in this case first) axis of the second array
 ### Assumes frequency is the last argument of the tensor to be transformed
+### We construct the PV of the real part using Hilbert transform and then simply use the old imaginary part such that we avoid numerical problems with using small eta
 def Kramers_Kronig(ws,Im_part):
 	### First we form the right Kramers Kronig tensor
 	Nws = len(ws)
-	kk_matrix = 1./(np.pi*float(Nws))*1./(np.tensordot(ws,np.ones_like(ws),axes=-1) - np.tensordot(np.ones_like(ws),ws,axes=-1) - 1.j*zero )
-	return Im_part@kk_matrix
+	dw = ws[1]-ws[0]
+	kk_matrix_real = np.zeros((Nws,Nws),dtype=complex)
+	for i in range(Nws):
+		for j in range(Nws):
+			#kk_matrix[i,j] = dw*((ws[j] - ws[i]) +1.j*zero)/(np.pi)*1./( (ws[j] - ws[i])**2 + zero**2) ### Sign is such that this will reconstruct the imaginary part to match the original imaginary part
+			if i != j:
+				kk_matrix_real[i,j] = dw/(np.pi)*1./(ws[j] - ws[i]) ### Sign is such that this will reconstruct the imaginary part to match the original imaginary part
+	return Im_part@kk_matrix_real + 1.j*Im_part
+
 
 ########################################
 ### Computing Magnon Greens Funciton ###
@@ -212,129 +231,6 @@ def RPA_kernel(kxs,kys,ws,Pi0,Pi1,J):
 	return kernel
 
 
-
-
-### This gives the Bogoliubov magnon propagator as a matrix 
-### The matrix elements are for the Bogoliubov spinor (a_q,b_-q^+, a_-q^+, b_q) components 
-
-
-
-##########################################
-### DEPRECATED METHODS FOR MONTE CARLO ###
-##########################################
-### This method uniformly samples the momentum points 
-"""
-def gen_kpts_uniform(ps):
-	N = int(ps.NMC)
-	kpts = rng.random((2,N))*2.*np.pi-np.pi*np.ones((2,N)) ### Random k points 
-
-	return kpts
-
-### This method samples momentum points according to the B2g form factor 
-def gen_kpts_B2g(ps):
-	N = int(ps.NMC)
-	### 
-	kpts = np.zeros((2,N))
-	num_accept = 0
-
-	while num_accept < N:
-		k = rng.random(2)*2.*np.pi-np.pi ### Random k point to propose
-
-		r = rng.random() ### accept probability
-		#r = 0.
-
-		if r < (B2g(k))**2:
-			kpts[:,num_accept] = k
-			num_accept += 1
-
-	return kpts
-
-### Hole spectral function for particular energy and momentum 
-### This will be obtained 
-def hole_spectrum(k,ps):
-	E=k[0]
-	t = ps.t
-	mu = ps.mu
-
-	W = 8.*t ### square lattice bandwidth in terms of hopping 
-	### This model uses a purely incoherent, flat band with box density of states, just for testing 
-
-	if E < -W/2. - mu/2.:
-		return 0.
-
-	elif E > W/2. -mu/2.:
-		return 0.
-
-	else: 
-		return 1./W 
-
-### This method will calculate the density of holes for a particular chemical potential, temperature, and spectral function 
-def calc_density(ps):
-	ks = hole_sample(ps) 
-
-	occs = 0.5-0.5*np.tanh(ks[0,:]/(2.*ps.T))
-
-	return np.mean(occs)
-
-### This method performs an MC sampling from the hole spectral function 
-### It returns N samples 
-### The return is in the form of [E,kx,ky] x N (3,N) shape 
-def hole_sample(ps):
-	N = int(ps.NMC)
-	samples = np.zeros((3,N))
-
-	num_accept = 0
-	while num_accept < N:
-		k = rng.random(2)*2.*np.pi-np.pi ### Random k point to propose
-		e = rng.random()*2.*ps.Emax- ps.Emax
-
-		r = rng.random() ### accept probability
-
-		if r < hole_spectrum(np.array([ e,k[0],k[1] ]),ps):
-			samples[:,num_accept] = e
-			samples[1:,num_accept] = k
-			num_accept += 1
-
-	return samples
-
-### This function evalutes the imaginary part of the RPA response function for a given magnon frequency and momentum  
-### These are passed as q = [w,qx,qy]
-def ImPi_MC(q,ps):
-
-	### We need to evaluate the integrals 
-	### integral_{E,p} iA(E,p) A(E+w,p+q) gamma_{p} [ gamma_{p}, gamma_{p+q} ] ( f(E)-f(E') )/(E - E'+w)
-
-	### We start by Monte Carlo sampling the hole momentum and energy k,E according to the spectral function 
-
-	N = int(ps.NMC)
-	k1s = hole_sample(ps) ### This generates a sample of energies and momenta for holes 
-
-	k2s = k1s + np.tensordot(q,np.ones(N),axes=-1) ### This is the sum of the energy and momenta 
-
-	### The only part that differs is the form factor 
-
-	integrands= np.zeros(N) 
-	for i in range(N):
-		integrands[i] = 2.*np.pi*S*ps.t**2*hole_spectrum(k2s[:,i],ps)*( 0.5*np.tanh(0.5*k1s[0,i]/ps.T) - 0.5*np.tanh(0.5*k2s[0,i]/ps.T) )
-
-	means = np.array([ np.mean(integrands*(Ag(k1s[1:,:]))**2 ), np.mean(integrands*Ag(k1s[1:,:])*Ag(k2s[1:,:]) )  ])
-	errs =  np.array([ np.std(integrands*(Ag(k1s[1:,:]))**2 ), np.std(integrands*Ag(k1s[1:,:])*Ag(k2s[1:,:]) )  ])
-
-	return means, errs
-
-### This function evalutes the imaginary part of the RPA response function for a given magnon frequency and momentum  
-### These are passed as q = [w,qx,qy]
-def ImPi_quad(q,ps):
-
-	### We need to evaluate the integrals 
-	### integral_{E,p} iA(E,p) A(E+w,p+q) gamma_{p} [ gamma_{p}, gamma_{p+q} ] ( f(E)-f(E') )/(E - E'+w)
-
-	integrand_1 = lambda w,kx,ky: 2.*np.pi*S*ps.t**2*Ag(np.array([kx,ky]))**2*hole_spectrum(np.array([w,kx,ky]),ps)*hole_spectrum(np.array([w,kx,ky])+q,ps)*( 0.5*np.tanh(0.5*w/ps.T) - 0.5*np.tanh(0.5*(w+q[0])/ps.T) )
-	integrand_2 = lambda w,kx,ky: 2.*np.pi*S*ps.t**2*Ag(np.array([kx,ky]))*Ag(np.array([kx,ky]) + q[1:])*hole_spectrum(np.array([w,kx,ky]),ps)*hole_spectrum(np.array([w,kx,ky])+q,ps)*( 0.5*np.tanh(0.5*w/ps.T) - 0.5*np.tanh(0.5*(w+q[0])/ps.T) )
-
-	return np.array([ intg.tplquad(integrand_1,-np.pi,np.pi,-np.pi,np.pi,-ps.Emax,ps.Emax)[0]/(4.*np.pi**2) , intg.tplquad(integrand_2,-np.pi,np.pi,-np.pi,np.pi,-ps.Emax,ps.Emax)[0]/(4.*np.pi**2) ])
-"""
-
 def main():
 
 	T = 0.11*t
@@ -345,32 +241,101 @@ def main():
 	dataDirectory = "../data/"
 
 	holesDirectory = dataDirectory+"hole_spectra/03252025/"
-	holesFile = "Hole_Spectral_functionJz0.0_alfa0.999_Nx20_Ny20"
+	holesFile = "Hole_Spectral_functionJz0.05_alfa0.999_Nx20_Ny20"
 
 	kxs,kys,ws,A = load_hole_spectrum(holesDirectory+holesFile)
 
-	A = np.abs(A) ### Sign errors in spectral function 
-	print(np.mean(A)) ### Should equal 1 if normalized 
-
-	#plt.imshow(np.transpose(A[:,0,:]),origin='lower',extent=[kxs[0],kxs[-1],ws[0],ws[-1]],aspect=0.3)
-	#plt.colorbar()
-	#plt.show()
+	plt.imshow(np.transpose(A[:,0,:]),origin='lower',extent=[kxs[0],kxs[-1],ws[0],ws[-1]],aspect=0.3)
+	plt.colorbar()
+	plt.xlabel(r'$k_x$')
+	plt.xticks([0,np.pi/2.,np.pi,3.*np.pi/2.,2.*np.pi],[r'0',r'$\pi/2$',r'$\pi$',r'$3\pi/2$',r'$2\pi$'])
+	plt.ylabel(r'$E/t$')
+	plt.show()
 
 	nmus = 100
-	mus = np.linspace(-20.*t,20.*t,nmus)
+	mus = np.linspace(-4.*t,4.*t,nmus)
 	deltas = np.zeros_like(mus)
 	for i in range(nmus):
 		deltas[i] = calc_density(kxs,kys,ws,A,mus[i],T)
+	
+	mu = -2.2*t
+	print(calc_density(kxs,kys,ws,A,mu,T))
+	plt.plot(mus,deltas)
+	plt.axvline(mu,linestyle='dashed',color='gray')
+	plt.xlabel(r'$\mu/t$')
+	plt.ylabel(r'$\delta$')
+	plt.show()
 
-	#plt.plot(mus,deltas)
-	#plt.xlabel(r'$\mu/t$')
-	#plt.ylabel(r'$\delta$')
-	#plt.show()
-
-	mu = -3.8*t
 	ImPi0,ImPi1 = calc_ImPi(kxs,kys,ws,A,mu,T)
 	Pi0 = Kramers_Kronig(ws,ImPi0)
 	Pi1 = Kramers_Kronig(ws,ImPi1)
+
+
+	plt.imshow(np.transpose(ImPi0[:,0,:]),origin='lower',extent=[kxs[0],kxs[-1],ws[0],ws[-1]],aspect=0.4,cmap='coolwarm')
+	plt.xlabel(r'$q_x$')
+	plt.xticks([0,np.pi/2.,np.pi,3.*np.pi/2.,2.*np.pi],[r'0',r'$\pi/2$',r'$\pi$',r'$3\pi/2$',r'$2\pi$'])
+	plt.ylabel(r'$\omega/t$')
+	plt.colorbar()
+	plt.show()
+
+	plt.imshow(np.transpose(ImPi1[:,0,:]),origin='lower',extent=[kxs[0],kxs[-1],ws[0],ws[-1]],aspect=0.4,cmap='coolwarm')
+	plt.xlabel(r'$q_x$')
+	plt.xticks([0,np.pi/2.,np.pi,3.*np.pi/2.,2.*np.pi],[r'0',r'$\pi/2$',r'$\pi$',r'$3\pi/2$',r'$2\pi$'])
+	plt.ylabel(r'$\omega/t$')
+	plt.colorbar()
+	plt.show()
+
+
+	indices = [ [0,0],[10,0],[0,10],[10,10]]
+
+	if False:
+		for i in indices:
+			label_string = r'$\mathbf{q}=($'+"{qx:0.2f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.2f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
+			plt.plot(ws/t,np.real(Pi0[i[0],i[1],:])/t,label=label_string)
+		
+		plt.xlabel(r'$\omega/t$')
+		plt.ylabel(r'Re$\Pi_0(\omega,\mathbf{q})/t$')
+		plt.legend()
+		#plt.savefig(figDirectory+"/RePi0.pdf",bbox_inches='tight')
+		plt.show()
+
+	clrs = ['red','green','blue','purple']
+	if True:
+		for j in range(len(indices)):
+			i = indices[j]
+			label_string = r'$\mathbf{q}=($'+"{qx:0.2f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.2f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
+			plt.plot(ws/t,np.imag(Pi0[i[0],i[1],:]),label=label_string,color=clrs[j])
+			plt.plot(ws/t,ImPi0[i[0],i[1],:],label=label_string,linestyle='dotted',color=clrs[j])
+		
+		plt.xlabel(r'$\omega/t$')
+		plt.ylabel(r'Im$\Pi_0(\omega,\mathbf{q})/t$')
+		plt.legend()
+		#plt.savefig(figDirectory+"/ImPi0.pdf",bbox_inches='tight')
+		plt.show()
+
+	if False:
+		for i in indices:
+			label_string = r'$\mathbf{q}=($'+"{qx:0.2f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.2f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
+			plt.plot(ws/t,np.real(Pi1[i[0],i[1],:])/t,label=label_string)
+		
+		plt.xlabel(r'$\omega/t$')
+		plt.ylabel(r'Re$\Pi_1(\omega,\mathbf{q})/t$')
+		plt.legend()
+		#plt.savefig(figDirectory+"/RePi1.pdf",bbox_inches='tight')
+		plt.show()
+
+	if False:
+		for i in indices:
+			label_string = r'$\mathbf{q}=($'+"{qx:0.2f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.2f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
+			plt.plot(ws/t,np.imag(Pi1[i[0],i[1],:])/t,label=label_string)
+		
+		plt.xlabel(r'$\omega/t$')
+		plt.ylabel(r'Im$\Pi_1(\omega,\mathbf{q})/t$')
+		plt.legend()
+		#plt.savefig(figDirectory+"/ImPi1.pdf",bbox_inches='tight')
+		plt.show()
+
+
 
 	magnon_kernel = LSW_kernel(kxs,kys,ws,J)
 	print(magnon_kernel.shape)
@@ -387,51 +352,6 @@ def main():
 	plt.ylim(-2.*J,2.*J)
 	plt.ylabel(r'$\omega/t$')
 	plt.show()
-	quit()
-
-
-	indices = [ [0,0],[10,0],[10,10],[-1,10],[-1,-1] ]
-
-	for i in indices:
-		label_string = r'$\mathbf{q}=($'+"{qx:0.0f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.0f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
-		plt.plot(ws/t,np.real(Pi0[i[0],i[1],:])/t,label=label_string)
-	
-	plt.xlabel(r'$\omega/t$')
-	plt.ylabel(r'Re$\Pi_0(\omega,\mathbf{q})/t$')
-	plt.legend()
-	plt.savefig(figDirectory+"/RePi0.pdf",bbox_inches='tight')
-	plt.show()
-
-	for i in indices:
-		label_string = r'$\mathbf{q}=($'+"{qx:0.0f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.0f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
-		plt.plot(ws/t,np.imag(Pi0[i[0],i[1],:])/t,label=label_string)
-	
-	plt.xlabel(r'$\omega/t$')
-	plt.ylabel(r'Im$\Pi_0(\omega,\mathbf{q})/t$')
-	plt.legend()
-	plt.savefig(figDirectory+"/ImPi0.pdf",bbox_inches='tight')
-	plt.show()
-
-	for i in indices:
-		label_string = r'$\mathbf{q}=($'+"{qx:0.0f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.0f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
-		plt.plot(ws/t,np.real(Pi1[i[0],i[1],:])/t,label=label_string)
-	
-	plt.xlabel(r'$\omega/t$')
-	plt.ylabel(r'Re$\Pi_1(\omega,\mathbf{q})/t$')
-	plt.legend()
-	plt.savefig(figDirectory+"/RePi1.pdf",bbox_inches='tight')
-	plt.show()
-
-	for i in indices:
-		label_string = r'$\mathbf{q}=($'+"{qx:0.0f}".format(qx=kxs[i[0]]/np.pi) + r'$\pi,$' + "{qy:0.0f}".format(qy=kys[i[1]]/np.pi) + r'$\pi)$' 
-		plt.plot(ws/t,np.imag(Pi1[i[0],i[1],:])/t,label=label_string)
-	
-	plt.xlabel(r'$\omega/t$')
-	plt.ylabel(r'Im$\Pi_1(\omega,\mathbf{q})/t$')
-	plt.legend()
-	plt.savefig(figDirectory+"/ImPi1.pdf",bbox_inches='tight')
-	plt.show()
-
 
 
 
